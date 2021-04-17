c4eef1331a83e4bb42266563dad4e48d075554ec
Ashton Sopher <ashtonsopher@gmail.com>
2021-04-17 15:09:57 -0400

    sample code segment

--- getEventDetails.py
+++ getEventDetails.py
@@ -1,1 +1,9 @@
 # borderline pseudocode
+page_token = None
+while True:
+    events = service.events().list(calendarId = 'primary', pageToken = page_token).execute()
+    for event in events['items']:
+        print event['summary']
+    page_token = events.get('nextPageToken')
+    if not page_token:
+        break
